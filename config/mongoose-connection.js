3397239a5bf143a027a9483f316a74d365a05a74
RohanKumar63 <rohankumar941536@gmail.com>
2024-11-25 14:39:24 +0530

    upadte local host to atlas

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,15 +1,36 @@
-const mongoose = require('mongoose');
-const dbgr = require("debug")("development:mongoose");
+// const mongoose = require('mongoose');
+// const dbgr = require("debug")("development:mongoose");
 
+// const config = require("config");
+
+// mongoose
+// .connect(`${config.get("MONGODB_URI")}` , {useNewUrlParser: true, useUnifiedTopology: true  })
+// .then(function(){
+//     console.log("connected");
+// })
+// .catch(function(err){
+//     console.log(err);
+// })
+
+// module.exports = mongoose.connection;
+
+
+//........................................updated code................................................
+
+const mongoose = require('mongoose');
+const debug = require("debug")("development:mongoose");
 const config = require("config");
 
+// Fetch the MongoDB URI from the configuration
+const MONGODB_URI = config.get("MONGODB_URI");
+
 mongoose
-.connect(`${config.get("MONGODB_URI")}/passwordManeger`)
-.then(function(){
-    dbgr("connected");
-})
-.catch(function(err){
-    dbgr(err);
-})
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+  });
 
 module.exports = mongoose.connection;
